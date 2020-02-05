0fdea43df36ba8eadbda3d8fd2de8b1dded31695
Robert Petrie <robertpetrie@outlook.com>
2020-02-04 20:56:43 -0500

    #1 Create the initial project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fix_it_tracker_back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        //  GET api/home
+        [HttpGet]
+        public string Index()
+        {
+            return "Home Controller";
+        }
+    }
+}
