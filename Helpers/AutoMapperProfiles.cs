97fc9cdca8b0dae090eaa27f5bf26fe98b38eaa4
Robert Petrie <robertpetrie@outlook.com>
2020-02-15 15:23:55 -0500

    #11 Helpers/AutoMapperProfiles.cs : Create the AutoMapperProfiles class
    This will be used to keep track of what models will be using AutoMapper

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace fix_it_tracker_back_end.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+        }
+    }
+}
