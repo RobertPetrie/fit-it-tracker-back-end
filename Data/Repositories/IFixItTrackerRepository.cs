7b42b5e5a5d2daa17f3f1eb8c0e9b70d91273e79
Robert Petrie <robertpetrie@outlook.com>
2020-02-18 21:13:57 -0500

    #15 Data\Repositories\IFixItTrackerRepository.cs : Create an interface for the repository.

---
+++ Data/Repositories/IFixItTrackerRepository.cs
@@ -0,0 +1,30 @@
+﻿using fix_it_tracker_back_end.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fix_it_tracker_back_end.Data.Repositories
+{
+    public interface IFixItTrackerRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+        Customer GetCustomer(int id);
+
+        IEnumerable<Fault> GetFaults();
+        Fault GetFault(int id);
+
+        IEnumerable<Item> GetItems();
+        Item GetItem(int id);
+
+        IEnumerable<ItemType> GetItemTypes();
+        ItemType GetItemType(int id);
+
+        IEnumerable<Repair> GetRepairs();
+        IEnumerable<Repair> GetCustomerRepairs(int id);
+        Repair GetRepair(int id);
+
+        IEnumerable<Resolution> GetResolutions();
+        Resolution GetResolution(int id);
+    }
+}
