58a05555dd8cd7490e41b44eb46a845268e1a034
Robert Petrie <robertpetrie@outlook.com>
2020-02-27 13:41:21 -0500

    #22 Helpers/Extensions.cs : Create an extension to customize error messages for front-end apps
    Doing this to expose the error messages for Front-End apps.  That way the custom error message will be displayed in the Front-End apps.

---
+++ Helpers/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fix_it_tracker_back_end.Helpers
+{
+    public static class Extensions
+    {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("fix-it-tracker-back-end-error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "fix-it-tracker-back-end-error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
