4fa9d74369cf3013806d26aa13f5d4114d761a6f
Robert Petrie <robertpetrie@outlook.com>
2020-03-04 11:40:04 -0500

    #24 Model/BindingTargets/FaultData.cs : Create the model binding for Fault

---
+++ Model/BindingTargets/FaultData.cs
@@ -0,0 +1,15 @@
+﻿namespace fix_it_tracker_back_end.Model.BindingTargets
+{
+    public class FaultData
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Fault Fault => new Fault
+        {
+            Name = Name,
+            Description = Description
+        };
+    }
+}
