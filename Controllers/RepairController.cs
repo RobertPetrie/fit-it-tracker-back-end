4d086027bfbd552a7e22582495751e4198bd860b
Robert Petrie <robertpetrie@outlook.com>
2020-03-28 15:47:02 -0400

    #28 Controllers/RepairController.cs : Create the UpdateRepair method
    With patch.ApplyTo EF performs change tracking on the objects it creates, which means that only modifications to the Repair objects will be written to the database.

--- Controllers/RepairController.cs
+++ Controllers/RepairController.cs
@@ -9,6 +9,7 @@ using fix_it_tracker_back_end.Model;
 using fix_it_tracker_back_end.Model.BindingTargets;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -120,6 +121,44 @@ namespace fix_it_tracker_back_end.Controllers
             var uri = Request != null ? Request.GetDisplayUrl().ToString() + createdRepair.RepairID : "";
 
             return Created(uri, "Repair Created");
+        }
+
+        /// <summary>
+        /// Updates a single repair.
+        /// </summary>
+        /// <param name="id">The repair Id you want to update.</param>
+        /// <param name="patch">The individual properties that you want to update.</param>
+        /// <returns>A message confirming that the repair has been updated</returns>
+        // PATCH api/repair/5
+        [HttpPatch("{id}")]
+        public ActionResult UpdateRepair(int id, [FromBody]JsonPatchDocument<RepairPatchData> patch)
+        {
+            Repair existingRepair = _dataContext.GetRepair(id);
+
+            if (existingRepair == null)
+            {
+                return BadRequest($"Repair ID: {id} doesn't exist");
+            }
+
+            RepairPatchData repairPatchData = new RepairPatchData { Repair = existingRepair };
+
+            patch.ApplyTo(repairPatchData, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (repairPatchData.DateCompleted != null)
+            {
+                if (DateTime.Compare(existingRepair.DateOpened, repairPatchData.DateCompleted.Value) > 0)
+                {
+                    return BadRequest("Date Completed is earlier than Date Opened");
+                }
+            }
+
+            _dataContext.UpdateRepair(repairPatchData.Repair);
+            return Ok("The repair has been updated.");
         }
     }
 }
