262077819ea1266bc6c080dace91b453bbe5cf96
Robert Petrie <robertpetrie@outlook.com>
2020-09-12 17:09:39 -0400

    #36 Create the UserRoles static class
    This is used to reference the names of roles so you don't have to hard code the names all the time.

---
+++ Helpers/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fix_it_tracker_back_end.Helpers
+{
+    public static class UserRoles
+    {
+        public const string AdminAccess = "Administrator";
+        public const string UserAccess = "User";
+    }
+}
